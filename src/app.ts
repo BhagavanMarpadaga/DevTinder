fa2e9eedca9318430bd043465ce74fe5e9cd1dcc
unknown <bhagavanmarpadaga@gmail.com>
2024-12-10 21:07:44 +0530

    added validations

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
 import express from "express";
 import { DBConnect } from "./config/database";
+import { IUserInput, User } from "./models/user";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+
+app.post("/user", async (req, res) => {
+  try {
+    console.log("body is ",req.body['firstName'])
+    // const user = new User<IUserInput>(req.body);
+    // await user.save();
+    // await User.findOneAndUpdate({firstName:"bhagavan"},{lastName:"M"},)
+    res.send("created successfully");
+  } catch (err) {
+    console.log("error is ", err);
+  } 
+});
 
 DBConnect()
   .then(() => {
-    
     app.listen(8000, () => {
       console.log("listing on port ", 8000);
     });
@@ -13,5 +27,3 @@ DBConnect()
   .catch((err: Error) => {
     console.log("Error while conneting to DB", err);
   });
-
-
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,26 +1,80 @@
-import mongoose from "mongoose";
+import mongoose, { Document } from "mongoose";
 
 const { Schema } = mongoose;
 
-export const userSchema = new Schema({
-  firstName: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-  },
-  emaiId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: String,
-  },
-  gender: {
-    type: String,
-  },
-});
+export interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  emaiId: string;
+  password: string;
+  age?: string; // Optional because `min` constraints won't enforce existence
+  gender: "Male" | "Female" | "Other"; // Enum constraint
+  photoUrl?: string; // Optional field
+  about?: string; // Optional field,
+  skills:Array<string>
+}
+export interface IUserInput {
+  firstName: string;
+  lastName: string;
+  emaiId: string;
+  password: string;
+  age?: string; // Optional because `min` constraints won't enforce existence
+  gender: "Male" | "Female" | "Other"; // Enum constraint
+  photoUrl?: string; // Optional field
+  about?: string; // Optional field
+  skills:Array<string>
+}
 
-export const User = mongoose.model("User",userSchema)
+export const userSchema = new Schema<IUser>(
+  {
+    firstName: {
+      type: String,
+      min: 4,
+      max: 40,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      min: 4,
+      max: 40,
+      required: true,
+    },
+    emaiId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      min: 8,
+      validate(value: string) {
+        if (value.length < 0) {
+          throw new Error("Password can not be empty");
+        }
+      },
+    },
+    age: {
+      type: String,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      enum: ["Male", "Female", "Other"],
+      required: true,
+    },
+    photoUrl: {
+      type: String,
+    },
+    about: {
+      type: String,
+    },
+    skills:{
+      type:[String]
+    }
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model<IUser>("User", userSchema);
